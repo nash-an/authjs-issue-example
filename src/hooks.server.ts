25099d62296e9d775e22f768ae1e9a8871a2e13a
Nash Anderson <nashradical.alt@gmail.com>
2023-08-25 17:06:09 -0700

    Create example

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,26 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import Google from "@auth/core/providers/google"
+import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private"
+
+const example_whitelist = [
+  // Your email address here
+  // Issue is reproducable when auth fails (so don't put your email here if reproducing)
+]
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+    Google({
+      clientId: GOOGLE_ID,
+      clientSecret: GOOGLE_SECRET
+    })
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      if (example_whitelist.includes(profile.email)) {
+        return true
+      }
+
+      return '/failed'
+    }
+  }
 })
